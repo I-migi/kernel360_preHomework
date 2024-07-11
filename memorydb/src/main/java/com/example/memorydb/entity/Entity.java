2c166a1b8e19ca6c21bb813cee7b2614228c99e7
sunwoo <sunn1017@naver.com>
2024-07-12 01:22:23 +0900

    to validation

--- memorydb/src/main/java/com/example/memorydb/entity/Entity.java
+++ memorydb/src/main/java/com/example/memorydb/entity/Entity.java
@@ -1,9 +1,14 @@
 package com.example.memorydb.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
 
-@Data
+
 public abstract class Entity implements PrimaryKey{
 
+    @Getter
+    @Setter
     private Long id;
 }
---
+++ memorydb/src/main/java/com/example/memorydb/user/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.memorydb.user.service;
+
+import com.example.memorydb.user.db.UserRepository;
+import com.example.memorydb.user.model.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    public UserEntity save(UserEntity user){
+        return userRepository.save(user);
+    }
+
+    public List<UserEntity> findAll(){
+        return userRepository.findAll();
+    }
+}
